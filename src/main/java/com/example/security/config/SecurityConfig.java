efe422dbe406a0b0566862109f441911e990f569
Ozodbek Khomidbekov <ozodbek.xomidbekov@gmail.com>
2022-10-30 19:54:24 +0900

    Initial commit

---
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.example.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("buzz")
+//                .password("infinity")
+//                .authorities("ROLE_USER")
+//                .and()
+//                .withUser("woody")
+//                .password("bullseye")
+//                .authorities("ROLE_USER");
+//    }
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                  .dataSource(dataSource);
+    }
+}
